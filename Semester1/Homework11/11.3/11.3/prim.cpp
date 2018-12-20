97042d6b09f89a225ed8cf181988df58a4363a51
Yuniya Kim <kim.yuniya@mail.ru>
2018-12-20 19:14:46 +0300

    Minor changes

--- Semester1/Homework11/11.3/11.3/prim.cpp
+++ Semester1/Homework11/11.3/11.3/prim.cpp
@@ -10,9 +10,6 @@ int **createGraph(const int size)
 	for (int i = 0; i < size; i++)
 	{
 		graph[i] = new int[size];
-	}
-	for (int i = 0; i < size; i++)
-	{
 		for (int j = 0; j < size; j++)
 		{
 			graph[i][j] = 10000000;
