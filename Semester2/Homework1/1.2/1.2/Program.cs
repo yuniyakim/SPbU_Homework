9dd8b26024240057614bc06b8595c1b39df6fd5a
Yuniya Kim <kim.yuniya@mail.ru>
2019-02-15 23:36:39 +0300

    Added 1.2

---
+++ Semester2/Homework1/1.2/1.2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _1._2
+{
+    class Program
+    {
+        private static int Fibonacci(int n)
+        {
+            if (n == 1 || n == 2)
+            {
+                return 1;
+            }
+
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number of Fibonacci");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= n; ++i)
+            {
+                Console.WriteLine($"{Fibonacci(i)}");
+            }
+        }
+    }
+}
