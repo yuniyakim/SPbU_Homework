67663b7f916961d34072c48bb137b5987bf7e996
Yuniya Kim <kim.yuniya@mail.ru>
2019-02-25 22:59:44 +0300

    Added 2.1

---
+++ Semester2/Homework2/2.1/2.1/List.cs
@@ -0,0 +1,194 @@
+﻿using System;
+
+namespace _2._1
+{
+    class List
+    {
+        public List()
+        {
+            head = null;
+            length = 0;
+        }
+
+        public bool IsEmpty()
+        {
+            return length == 0;
+        }
+
+        public bool IsContained(int position)
+        {
+            return !(IsEmpty() || position > length || position < 1);
+        }
+
+        public int Length()
+        {
+            return length;
+        }
+
+        public void print()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("List is empty");
+            }
+            else
+            {
+                Node temp = head;
+                for (int i = 0; i < length; ++i)
+                {
+                    Console.WriteLine($"Value: {temp.Value}, position: {temp.Position}");
+                    if (temp.Next != null)
+                    {
+                        temp = temp.Next;
+                    }
+                }
+            }
+        }
+
+        private void Renumbering()
+        {
+            Node temp = head;
+            for (int i = 1; i <= length; ++i)
+            {
+                temp.Position = i;
+                if (temp.Next != null)
+                {
+                    temp = temp.Next;
+                }
+            }
+        }
+
+        public void Push(string value, int position)
+        {
+            if (IsEmpty())
+            {
+                head = new Node(value, 1);
+                ++length;
+            }
+            else if (position > length + 1 || position < 1)
+            {
+                Console.WriteLine("List overflow");
+            }
+            else
+            {
+                Node newElement = new Node(value, position);
+                if (position == 1)
+                {
+                    newElement.Next = head;
+                    head = newElement;
+                    ++length;
+                    Renumbering();
+                }
+                else
+                {
+                    Node temp = head;
+                    for (int i = 1; i < position - 1; ++i)
+                    {
+                        if (temp.Next != null)
+                        {
+                            temp = temp.Next;
+                        }
+                    }
+                    newElement.Next = temp.Next;
+                    temp.Next = newElement;
+                    ++length;
+                    Renumbering();
+                }
+            }
+        }
+
+        public void Delete(int position)
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("List is empty");
+            }
+            else if (!IsContained(position))
+            {
+                Console.WriteLine("List overflow");
+            }
+            else
+            {
+                if (position == 1)
+                {
+                    if (length == 1)
+                    {
+                        head = null;
+                    }
+                    else
+                    {
+                        head = head.Next;
+                    }
+                }
+                else
+                {
+                    Node temp = head;
+                    for (int i = 1; i < position - 1; ++i)
+                    {
+                        temp = temp.Next;
+                    }
+                    if (position == length)
+                    {
+                        temp.Next = null;
+                    }
+                    else
+                    {
+                        temp.Next = temp.Next.Next;
+                    }
+                }
+                --length;
+                Renumbering();
+            }
+        }
+
+        public void GetValue(int position)
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("List is empty");
+            }
+            else if (!IsContained(position))
+            {
+                Console.WriteLine("List overflow");
+            }
+            else
+            {
+                Node temp = head;
+                for (int i = 1; i < position; ++i)
+                {
+                    temp = temp.Next;
+                }
+                Console.WriteLine($"Value: {temp.Value}, position: {temp.Position}");
+            }
+        }
+
+        public void SetValue(string value, int position)
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("List is empty");
+            }
+            else if (!IsContained(position))
+            {
+                Console.WriteLine("List overflow");
+            }
+            else
+            {
+                Node temp = head;
+                for (int i = 1; i < position; ++i)
+                {
+                    temp = temp.Next;
+                }
+                temp.Value = value;
+            }
+        }
+
+        private Node head;
+        private int length;
+    }
+}
+
+/* Написать связный список в виде класса. От списка хочется:
+- Добавлять/удалять элемент по произвольной позиции, задаваемой целым числом
+- Узнавать размер, проверять на пустоту
+- Получать или устанавливать значение элемента по позиции, задаваемой целым числом */
---
+++ Semester2/Homework2/2.1/2.1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace _2._1
+{
+    class Program
+    {
+        static void Main()
+        {
+            List list = new List();
+            if (list.IsEmpty())
+            {
+                Console.WriteLine("Empty");
+            }
+            list.Delete(4);
+            list.Push("one", 4);
+            list.Delete(2);
+            if (!list.IsEmpty())
+            {
+                Console.WriteLine("Not empty");
+            }
+            list.Push("three", 2);
+            list.print();
+            Console.WriteLine($"Length is {list.Length()}");
+            list.Push("four", 1);
+            list.Push("five", 3);
+            list.Delete(1);
+            list.Push("one", 3);
+            list.Delete(4);
+            list.Push("six", 2);
+            list.Push("two", 7);
+            list.print();
+            list.Push("seven", 5);
+            Console.WriteLine($"Length is {list.Length()}");
+            list.Delete(4);
+            if (list.IsContained(4))
+            {
+                Console.WriteLine("Contained");
+            }
+            list.print();
+            list.SetValue("check", 3);
+            list.SetValue("one-one", 1);
+            list.GetValue(2);
+            list.GetValue(1);
+            list.GetValue(4);
+            if (!list.IsContained(7))
+            {
+                Console.WriteLine("Not contained");
+            }
+            list.print();
+        }
+    }
+}
+
+/* Написать связный список в виде класса. От списка хочется:
+- Добавлять/удалять элемент по произвольной позиции, задаваемой целым числом
+- Узнавать размер, проверять на пустоту
+- Получать или устанавливать значение элемента по позиции, задаваемой целым числом */
