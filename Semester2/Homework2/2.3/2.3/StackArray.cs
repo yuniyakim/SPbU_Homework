633d54901ecfba7763987593ea083a92091e6cc4
Yuniya Kim <kim.yuniya@mail.ru>
2019-02-27 23:36:21 +0300

    Added 2.3

---
+++ Semester2/Homework2/2.3/2.3/StackArray.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace _2._3
+{
+    class StackArray : IStack
+    {
+        public StackArray()
+        {
+            length = 0;
+            stackArray = new int[size];
+        }
+        public void Push(int value)
+        {
+            if (length == size)
+            {
+                int[] newStack = new int[size * 2];
+                size *= 2;
+                stackArray.CopyTo(newStack, 0);
+                stackArray = newStack;
+            }
+            stackArray[length] = value;
+            ++length;
+        }
+
+        public int Pop()
+        {
+            if (IsEmpty())
+            {
+                throw new Exception("Stack is empty");
+            }
+            var pop = stackArray[length - 1];
+            stackArray[length - 1] = 0;
+            --length;
+            return pop;
+        }
+
+        public bool IsEmpty()
+        {
+            return length == 0;
+        }
+
+        private int size = 10;
+        private int length;
+        private int[] stackArray;
+    }
+}
+
+// Реализовать стековый калькулятор(класс, реализующий выполнение операций +, -, *, / над арифметическим выражением в виде строки в постфиксной записи). 
+// Стек реализовать двумя способами(например, массивом или списком) в двух разных классах на основе одного интерфейса.
+// Стековый калькулятор должен знать только про интерфейс стека.
---
+++ Semester2/Homework2/2.3/2.3/StackList.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _2._3
+{
+    class StackList : IStack
+    {
+        public StackList()
+        {
+            stackHead = null;
+            length = 0;
+        }
+
+        public void Push(int value)
+        {
+            var newNode = new Node(value);
+            newNode.Next = stackHead;
+            stackHead = newNode;
+            ++length;
+        }
+
+        public int Pop()
+        {
+            if (IsEmpty())
+            {
+                throw new Exception("Stack is empty");
+            }
+            var pop = stackHead.Value;
+            stackHead = stackHead.Next;
+            --length;
+            return pop;
+        }
+
+        public bool IsEmpty()
+        {
+            return length == 0;
+        }
+
+        private int length;
+        private Node stackHead;
+    }
+}
+
+// Реализовать стековый калькулятор(класс, реализующий выполнение операций +, -, *, / над арифметическим выражением в виде строки в постфиксной записи). 
+// Стек реализовать двумя способами(например, массивом или списком) в двух разных классах на основе одного интерфейса.
+// Стековый калькулятор должен знать только про интерфейс стека.
