ca8c44fd7fba148813076660f0b42b6d0c2ade95
Yuniya Kim <kim.yuniya@mail.ru>
2019-03-16 14:24:38 +0300

    Added Tree and Node

---
+++ Semester2/Homework4/4.1/4.1/Tree.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _4._1
+{
+    /// <summary>
+    /// Tree on the nodes
+    /// </summary>
+    public class Tree
+    {
+        /// <summary>
+        /// Node of the tree
+        /// </summary>
+        public class Node
+        {
+            /// <summary>
+            /// Node constructor
+            /// </summary>
+            /// <param name="value">Value to add into the node</param>
+            /// <param name="leftChild">Left child of this node</param>
+            /// <param name="rigthChild">Rigth child of this node</param>
+            public Node(char value, Node leftChild, Node rigthChild)
+            {
+                this.value = value;
+                this.leftChild = leftChild;
+                this.rightChild = rigthChild;
+            }
+
+            private char value { get; set; }
+            private Node leftChild { get; set; }
+            private Node rightChild { get; set; }
+        }
+
+        public Tree()
+        {
+            head = null;
+        }
+
+        private Node head;
+    }
+}
