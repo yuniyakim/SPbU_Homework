f93a02ce1203232581293eaca651bb9ed487d89b
Yuniya Kim <kim.yuniya@mail.ru>
2019-05-06 22:08:59 +0300

    Added 7.2

---
+++ Semester2/Homework7/7.2/7.2/Clock.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace _7._2
+{
+    partial class Clock
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.time = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // time
+            // 
+            this.time.AutoSize = true;
+            this.time.Font = new System.Drawing.Font("Calibri", 46.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.time.Location = new System.Drawing.Point(12, 9);
+            this.time.Name = "time";
+            this.time.Size = new System.Drawing.Size(322, 95);
+            this.time.TabIndex = 0;
+            this.time.Text = "00:00:00";
+            // 
+            // Clock
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(344, 113);
+            this.Controls.Add(this.time);
+            this.MaximumSize = new System.Drawing.Size(362, 160);
+            this.MinimumSize = new System.Drawing.Size(362, 160);
+            this.Name = "Clock";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Clock";
+            this.Load += new System.EventHandler(this.Clock_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label time;
+    }
+}
+
---
+++ Semester2/Homework7/7.2/7.2/Clock.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace _7._2
+{
+    public partial class Clock : Form
+    {
+
+        private Timer timer = new Timer();
+        public Clock()
+        {
+            InitializeComponent();
+        }
+
+        private void Clock_Load(object sender, EventArgs e)
+        {
+            timer.Interval = 1000;
+            timer.Tick += new EventHandler(Clock_Tick);
+            timer.Start();
+        }
+
+        private void Clock_Tick(object sender, EventArgs e)
+        {
+            string currentTime = "";
+            var hours = DateTime.Now.Hour;
+            var minutes = DateTime.Now.Minute;
+            var seconds = DateTime.Now.Second;
+
+            if (hours < 10)
+            {
+                currentTime += "0";
+            }
+            currentTime += hours;
+            currentTime += ":";
+
+            if (minutes < 10)
+            {
+                currentTime += "0";
+            }
+            currentTime += minutes;
+            currentTime += ":";
+
+            if (seconds < 10)
+            {
+                currentTime += "0";
+            }
+            currentTime += seconds;
+
+            time.Text = currentTime;
+        }
+    }
+}
