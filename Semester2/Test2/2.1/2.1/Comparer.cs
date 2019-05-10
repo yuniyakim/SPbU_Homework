faef3e1f0f53d1c491875b6161913331f4b52fa4
Yuniya Kim <kim.yuniya@mail.ru>
2019-05-10 14:30:44 +0300

    Added Comparer

---
+++ Semester2/Test2/2.1/2.1/Comparer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2._1
+{
+    public class Comparer<T> : IComparer<List<T>>
+    {
+        public int Compare(List<T> list1, List<T> list2)
+        {
+            if (list1.Count > list2.Count)
+            {
+                return 1;
+            }
+            else if (list1.Count < list2.Count)
+            {
+                return -1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
