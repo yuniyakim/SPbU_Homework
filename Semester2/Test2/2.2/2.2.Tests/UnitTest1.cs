5ad16dad85610d7f556304d59a96d87e8e9b6442
Yuniya Kim <kim.yuniya@mail.ru>
2019-05-17 13:24:16 +0300

    Created 2.2 and its' tests

---
+++ Semester2/Test2/2.2/2.2.Tests/UnitTest1.cs
@@ -0,0 +1,19 @@
+using System;
+using NUnit.Framework;
+
+namespace _2._2
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ Semester2/Test2/2.2/2.2/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace _2._2
+{
+    class Program
+    {
+        static void Main()
+        {
+        }
+    }
+}
