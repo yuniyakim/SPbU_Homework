ed749693a554a340f982cf8254bf60a3e3042396
Yuniya Kim <kim.yuniya@mail.ru>
2019-05-17 14:11:05 +0300

    Added Swap and Sort

---
+++ Semester2/Test2/2.2/2.2/BubbleSort.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2._2
+{
+    public class BubbleSort
+    {
+        public List<T> Sort<T>(List<T> list, IComparer<T> comparer)
+        {
+            for (int i = 0; i < list.Count; ++i)
+            {
+                for (int j = 1; j < list.Count - 1; ++j)
+                {
+                    if (comparer.Compare(list[j], list[j - 1]) < 0)
+                    {
+                        Swap(list[j], list[j - 1]);
+                    }
+                }
+            }
+
+            return list;
+        }
+
+        private void Swap<T>(T element1, T element2)
+        {
+            var temp = element1;
+            element1 = element2;
+            element2 = temp;
+        }
+    }
+}
+
+// Реализовать generic метод пузырьковой сортировки. Метод должен принимать список объектов произвольного типа и объект, позволяющий их сравнивать. 
+// Результатом должен быть список, отсортированный в соответствии с порядком, заданным объектом-компаратором.
--- Semester2/Test2/2.2/2.2/Program.cs
+++ Semester2/Test2/2.2/2.2/Program.cs
@@ -2,7 +2,7 @@
 
 namespace _2._2
 {
-    class Program
+    public class Program
     {
         static void Main()
         {
