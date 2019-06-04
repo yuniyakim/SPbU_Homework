28471e1ba73cdfe05751d49c646ad847fd5058f0
Yuniya Kim <kim.yuniya@mail.ru>
2019-06-04 22:25:37 +0300

    Created tests

---
+++ Semester2/Homework7/7.1/7.1.Tests/CalculatorTests.cs
@@ -0,0 +1,19 @@
+using System;
+using NUnit.Framework;
+
+namespace _7._1
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
