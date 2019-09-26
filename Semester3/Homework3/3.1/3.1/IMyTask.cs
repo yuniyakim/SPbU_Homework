78bac0a643051a64babf9b6e6aca9fe74f227b1c
Yuniya Kim <kim.yuniya@mail.ru>
2019-09-26 22:51:39 +0300

    Added IMyTask and created MyThreadPool

---
+++ Semester3/Homework3/3.1/3.1/IMyTask.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _3._1
+{
+    /// <summary>
+    /// Task interface
+    /// </summary>
+    public interface IMyTask<TResult>
+    {
+        /// <summary>
+        /// Defines whether or not task is completed
+        /// </summary>
+        /// <returns>True if task is completed, false otherwise</returns>
+        bool IsCompleted();
+        /// <summary>
+        /// Returns result of task's completion
+        /// </summary>
+        /// <returns>Task's result</returns>
+        TResult Result();
+        /// <summary>
+        /// Returns a new task which is going to me completed
+        /// </summary>
+        /// <param name="func">Incoming function</param>
+        /// <returns>New task</returns>
+        IMyTask<TNewResult> ContinueWith<TNewResult>(Func<TResult, TNewResult> func);
+    }
+}
---
+++ Semester3/Homework3/3.1/3.1/MyThreadPool.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Threading;
+
+namespace _3._1
+{
+    public class MyThreadPool
+    {
+    }
+}
