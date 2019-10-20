c9bf7f98c0cda857aa73f73e888484fab8bf0956
Yuniya Kim <kim.yuniya@mail.ru>
2019-10-20 14:41:36 +0300

    Reorganised again and added some testing code

---
+++ Semester3/Homework5/5.1/Server/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Server
+{
+    public class Program
+    {
+        const int port = 8888;
+        static TcpListener listener;
+        static void Main()
+        {
+            try
+            {
+                listener = new TcpListener(IPAddress.Parse("127.0.0.1"), port);
+                listener.Start();
+                Console.WriteLine("Waiting for connections...");
+
+                while (true)
+                {
+                    TcpClient client = listener.AcceptTcpClient();
+                    var clientObject = new Server(client);
+
+                    // создаем новый поток для обслуживания нового клиента
+                    Thread clientThread = new Thread(new ThreadStart(clientObject.Process));
+                    clientThread.Start();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (listener != null)
+                    listener.Stop();
+            }
+        }
+    }
+}
