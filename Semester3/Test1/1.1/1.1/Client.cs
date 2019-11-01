e2a41868bb9ae1594370f0f4876dffc55e4cb6e4
Yuniya Kim <kim.yuniya@mail.ru>
2019-11-01 15:24:14 +0300

    Wrote something

---
+++ Semester3/Test1/1.1/1.1/Client.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _1._1
+{    public class Client
+    {
+        protected internal NetworkStream Stream { get; private set; }
+        string userName;
+        TcpClient tcpClient;
+        private const string host = "127.0.0.1";
+        private static NetworkStream stream;
+
+        private int port;
+        private Server server;
+
+        public Client(int port, TcpClient tcpClient, Server server)
+        {
+            this.port = port;
+            this.tcpClient = tcpClient;
+            this.server = server;
+            server.AddConnection(this);
+        }
+
+        public async Task Process()
+        {
+            try
+            {
+                Stream = tcpClient.GetStream();
+                var message = await GetMessage();
+                userName = message;
+                await server.SendMessage("Connection established.", tcpClient);
+                Console.WriteLine(message);
+
+                while (true)
+                {
+                    try
+                    {
+                        message = await GetMessage();
+                        Console.WriteLine($"{userName}: {message}");
+                        await server.SendMessage(message, tcpClient);
+                    }
+                    catch
+                    {
+                        Console.WriteLine($"{userName}: left chat");
+                        await server.SendMessage(message, tcpClient);
+                        break;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                server.RemoveConnection();
+                Close();
+            }
+        }
+
+        protected internal async Task<string> GetMessage()
+        {
+            try
+            {
+                using (Stream)
+                {
+                    var reader = new StreamReader(Stream);
+                    var message = await reader.ReadLineAsync();
+                    return message;
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+                (tcpClient as TcpClient).Close();
+                return null;
+            }
+        }
+
+        protected internal void Close()
+        {
+            if (Stream != null)
+                Stream.Close();
+            if (tcpClient != null)
+                tcpClient.Close();
+        }
+
+        protected internal async Task SendMessage()
+        {
+            Console.WriteLine("Enter your message: ");
+
+            while (true)
+            {
+                var  message = Console.ReadLine();
+
+                try
+                {
+                    using (Stream)
+                    {
+                        var streamWriter = new StreamWriter(Stream);
+                        await streamWriter.WriteLineAsync(message);
+                    }
+                }
+                catch (IOException e)
+                {
+                    Console.WriteLine(e.Message);
+                    (tcpClient as TcpClient).Close();
+                }
+            }
+        }
+
+        protected internal async Task ReceiveMessage()
+        {
+            while (true)
+            {
+                try
+                {
+                    string message;
+                    using (Stream)
+                    {
+                        var streamReader = new StreamReader(Stream);
+                        message = await streamReader.ReadLineAsync();
+                    }
+                    Console.WriteLine(message);
+                }
+                catch
+                {
+                    Console.WriteLine("Connection lost");
+                    Console.ReadLine();
+                    Disconnect();
+                }
+            }
+        }
+
+        protected internal void Disconnect()
+        {
+            if (stream != null)
+            {
+                stream.Close();
+            }
+            if (tcpClient != null)
+            {
+                tcpClient.Close();
+            }
+            Environment.Exit(0);
+        }
+
+        protected internal async Task ClientProcess()
+        {
+            Console.Write("Enter your name: ");
+            userName = Console.ReadLine();
+            tcpClient = new TcpClient();
+            try
+            {
+                tcpClient.Connect(host, port);
+                var writer = new StreamWriter(tcpClient.GetStream());
+                string message = userName;
+                await writer.WriteLineAsync(message);
+                await ReceiveMessage();
+                Console.WriteLine($"Hello, {userName}");
+                await SendMessage();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Disconnect();
+            }
+        }
+    }
+}
---
+++ Semester3/Test1/1.1/1.1/Server.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _1._1
+{
+    public class Server
+    {
+        private TcpListener tcpListener;
+        private Client client;
+        private int port;
+        static Thread listenThread;
+
+        protected internal void AddConnection(Client clientObject)
+        {
+            client= clientObject;
+        }
+        protected internal void RemoveConnection()
+        {
+            client = null;
+        }
+
+        protected internal async Task Listen()
+        {
+            try
+            {
+                tcpListener = new TcpListener(IPAddress.Any, port);
+                tcpListener.Start();
+                Console.WriteLine("Server launched. Waiting for connections.");
+
+                while (true)
+                {
+                    var tcpClient = tcpListener.AcceptTcpClient();
+
+                    client = new Client(port, tcpClient, this);
+                    await client.Process();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Disconnect();
+            }
+        }
+
+        protected internal async Task SendMessage(string message, TcpClient tcpClient)
+        {
+            try
+            {
+                using (var stream = (tcpClient as TcpClient).GetStream())
+                {
+                    var streamWriter = new StreamWriter(stream);
+                    await streamWriter.WriteLineAsync(message);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+                (tcpClient as TcpClient).Close();
+            }
+        }
+
+        protected internal void Disconnect()
+        {
+            tcpListener.Stop();
+            client.Close();
+            Environment.Exit(0);
+        }
+
+        public async Task ServerProcess()
+        {
+            try
+            {
+                await Listen();
+            }
+            catch (Exception ex)
+            {
+                Disconnect();
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
