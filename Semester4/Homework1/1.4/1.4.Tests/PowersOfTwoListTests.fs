4e9ae00777f17c4c4bbcdfc0b68dfa88a00d506c
Yuniya Kim <kim.yuniya@mail.ru>
2020-02-16 23:35:59 +0300

    Fixed powTwo and added some tests

--- Semester4/Homework1/1.4/1.4.Tests/PowersOfTwoListTests.fs
+++ Semester4/Homework1/1.4/1.4.Tests/PowersOfTwoListTests.fs
@@ -1,7 +1,21 @@
 module PowerOfTwoListTests
 
 open NUnit.Framework
+open PowersOfTwoList
 
 [<Test>]
-let Test1 () =
-    Assert.Pass()
+let PositivePowTwoLest () =
+    Assert.AreEqual(Some(2), powTwo 1)
+    Assert.AreEqual(Some(32), powTwo 5)
+    Assert.AreEqual(Some(1024), powTwo 10)
+    Assert.AreEqual(Some(16384), powTwo 14)
+
+[<Test>]
+let ZeroPowTwoLest () =
+    Assert.AreEqual(Some(1), powTwo 0)
+
+[<Test>]
+let NegativePowTwoLest () =
+    Assert.AreEqual(None, powTwo -1)
+    Assert.AreEqual(None, powTwo -18)
+    Assert.AreEqual(None, powTwo -999)
--- Semester4/Homework1/1.4/1.4/PowersOfTwoList.fs
+++ Semester4/Homework1/1.4/1.4/PowersOfTwoList.fs
@@ -2,24 +2,24 @@
 
 open System
 
-let powTwo power =
+let powTwo n =
     let rec pow acc i =
-        if power < 0 then
+        if n < 0 then
             None
-        else if power = 0 then
+        else if n = 0 then
             Some(1)
-        else if power = i then
-            Some(acc)
+        else if n = i then
+            Some(acc * 2)
         else
             pow
                 (acc * 2)
                 (i + 1)
     pow 1 1
 
-let powersOfTwoList n m =
-    let rec powersOfTwo ls acc i =
+//let powersOfTwoList n m =
+//    let rec powersOfTwo ls acc i =
         
-    powersOfTwo
-        ([])
-        (2)
-        (1)
+//    powersOfTwo
+//        ([])
+//        (2)
+//        (1)
