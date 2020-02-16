9c9e65283e8481ed5be7391901f5c44be2f168e2
Yuniya Kim <kim.yuniya@mail.ru>
2020-02-16 17:38:14 +0300

    Added 1.1

---
+++ Semester4/Homework1/1.1/1.1/Factorial.fs
@@ -0,0 +1,17 @@
+﻿module Factorial
+
+open System
+
+let factorial n =
+    let rec fact acc i =
+        if n < 0 then
+            None
+        else if n = 0 then
+            Some(1)
+        else if i = n then
+            Some(acc * i)
+        else
+            fact
+                (acc * i)
+                (i + 1)
+    fact 1 1
