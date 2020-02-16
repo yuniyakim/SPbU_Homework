afd11e8d7e01a1b4058986cc878cb1c061bab16f
Yuniya Kim <kim.yuniya@mail.ru>
2020-02-16 22:18:16 +0300

    Changed the names of tests

--- Semester4/Homework1/1.3/1.3.Tests/ReverseListTests.fs
+++ Semester4/Homework1/1.3/1.3.Tests/ReverseListTests.fs
@@ -4,12 +4,12 @@ open NUnit.Framework
 open ReverseList
 
 [<Test>]
-let SimpleListReverseTest () =
+let ReverseListTest () =
     Assert.AreEqual(["something"], reverseList ["something"])
     Assert.AreEqual([3; 2; 1], reverseList [1; 2; 3])
     Assert.AreEqual([0; -2; 44; 9], reverseList [9; 44; -2; 0])
     Assert.AreEqual(["one"; "two"; "three"; "four"; "five"; "one"; "six"], reverseList ["six"; "one"; "five"; "four"; "three"; "two"; "one"])
 
 [<Test>]
-let EmptyListReverseTest () =
+let ReverseEmptyListTest () =
     Assert.AreEqual([], [])
