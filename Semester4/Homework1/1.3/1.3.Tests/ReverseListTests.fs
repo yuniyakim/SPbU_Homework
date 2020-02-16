7f95cdc81e490f86a5d5a28ea64d2041b08da7a6
Yuniya Kim <kim.yuniya@mail.ru>
2020-02-16 22:17:33 +0300

    Added reverseList and some tests

--- Semester4/Homework1/1.3/1.3.Tests/ReverseListTests.fs
+++ Semester4/Homework1/1.3/1.3.Tests/ReverseListTests.fs
@@ -3,10 +3,13 @@ module ReverseListTests
 open NUnit.Framework
 open ReverseList
 
-[<SetUp>]
-let Setup () =
-    ()
+[<Test>]
+let SimpleListReverseTest () =
+    Assert.AreEqual(["something"], reverseList ["something"])
+    Assert.AreEqual([3; 2; 1], reverseList [1; 2; 3])
+    Assert.AreEqual([0; -2; 44; 9], reverseList [9; 44; -2; 0])
+    Assert.AreEqual(["one"; "two"; "three"; "four"; "five"; "one"; "six"], reverseList ["six"; "one"; "five"; "four"; "three"; "two"; "one"])
 
 [<Test>]
-let Test1 () =
-    Assert.Pass()
+let EmptyListReverseTest () =
+    Assert.AreEqual([], [])
