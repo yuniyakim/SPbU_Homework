663c01add252d6bac61fc150296e616780193b64
Yuniya Kim <kim.yuniya@mail.ru>
2020-02-16 18:16:54 +0300

    Added 1.2

---
+++ Semester4/Homework1/1.2/1.2/Fibonacci.fs
@@ -0,0 +1,18 @@
+﻿module Fibonacci
+
+open System
+
+let fibonacci n =
+    let rec fib acc1 acc2 i =
+        if n < 0 then
+            None
+        else if n = 0 then
+            Some(0)
+        else if n = i then
+            Some(acc2)
+        else
+            fib
+                (acc2)
+                (acc1 + acc2)
+                (i + 1)
+    fib 0 1 1
