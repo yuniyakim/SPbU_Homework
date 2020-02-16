9587d22f28bf8ece11f94f872e5e4439e5117cd0
Yuniya Kim <kim.yuniya@mail.ru>
2020-02-16 23:12:42 +0300

    Added powTwo function

--- Semester4/Homework1/1.4/1.4/PowersOfTwoList.fs
+++ Semester4/Homework1/1.4/1.4/PowersOfTwoList.fs
@@ -2,5 +2,24 @@
 
 open System
 
-let powersOfTwoList ls =
+let powTwo power =
+    let rec pow acc i =
+        if power < 0 then
+            None
+        else if power = 0 then
+            Some(1)
+        else if power = i then
+            Some(acc)
+        else
+            pow
+                (acc * 2)
+                (i + 1)
+    pow 1 1
 
+let powersOfTwoList n m =
+    let rec powersOfTwo ls acc i =
+        
+    powersOfTwo
+        ([])
+        (2)
+        (1)
