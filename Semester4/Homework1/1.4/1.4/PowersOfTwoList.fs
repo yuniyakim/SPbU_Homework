8279232408fee85275a1c8b5a84ea08297a63a10
Yuniya Kim <kim.yuniya@mail.ru>
2020-02-17 12:17:34 +0300

    Finished powersOfTwoList

--- Semester4/Homework1/1.4/1.4/PowersOfTwoList.fs
+++ Semester4/Homework1/1.4/1.4/PowersOfTwoList.fs
@@ -11,15 +11,19 @@ let powTwo n =
         else if n = i then
             Some(acc * 2)
         else
-            pow
-                (acc * 2)
-                (i + 1)
+            pow (acc * 2) (i + 1)
+
     pow 1 1
 
-//let powersOfTwoList n m =
-//    let rec powersOfTwo ls acc i =
-        
-//    powersOfTwo
-//        ([])
-//        (2)
-//        (1)
+let powersOfTwoList n m =
+    let rec powersOfTwo acc i =
+        if n < 0 || m < 0 then
+            None
+        else if n = 0 && m = 0 then
+            Some([1])
+        else if i = n then
+            Some(acc)
+        else
+            powersOfTwo ((List.head acc / 2) :: acc) (i - 1)
+
+    powersOfTwo ([]) (m)
