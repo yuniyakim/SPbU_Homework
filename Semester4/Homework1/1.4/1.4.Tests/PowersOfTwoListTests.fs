6d3f96f34d03e56314b8664934d523dbbe04afbb
Yuniya Kim <kim.yuniya@mail.ru>
2020-02-18 11:56:17 +0300

    Improved powTwo, fixed powersOfTwoList and added some tests

--- Semester4/Homework1/1.4/1.4.Tests/PowersOfTwoListTests.fs
+++ Semester4/Homework1/1.4/1.4.Tests/PowersOfTwoListTests.fs
@@ -4,18 +4,34 @@ open NUnit.Framework
 open PowersOfTwoList
 
 [<Test>]
-let PositivePowTwoLest () =
-    Assert.AreEqual(Some(2), powTwo 1)
-    Assert.AreEqual(Some(32), powTwo 5)
-    Assert.AreEqual(Some(1024), powTwo 10)
-    Assert.AreEqual(Some(16384), powTwo 14)
+let PositivePowTwoTest () =
+    Assert.AreEqual(2, powTwo 1)
+    Assert.AreEqual(32, powTwo 5)
+    Assert.AreEqual(1024, powTwo 10)
+    Assert.AreEqual(16384, powTwo 14)
 
 [<Test>]
-let ZeroPowTwoLest () =
-    Assert.AreEqual(Some(1), powTwo 0)
+let ZeroPowTwoTest () =
+    Assert.AreEqual(1, powTwo 0)
 
 [<Test>]
-let NegativePowTwoLest () =
-    Assert.AreEqual(None, powTwo -1)
-    Assert.AreEqual(None, powTwo -18)
-    Assert.AreEqual(None, powTwo -999)
+let NegativePowTwoTest () =
+    Assert.AreEqual(-1, powTwo -1)
+    Assert.AreEqual(-1, powTwo -18)
+    Assert.AreEqual(-1, powTwo -999)
+
+[<Test>]
+let NegativeArgsPowersOfTwoTest () =
+    Assert.AreEqual(None, powersOfTwoList -1 6)
+    Assert.AreEqual(None, powersOfTwoList 287 -18)
+    Assert.AreEqual(None, powersOfTwoList -64 -2)
+
+[<Test>]
+let ZeroArgsPowersOfTwoTest () =
+    Assert.AreEqual(Some[1], powersOfTwoList 0 0)
+
+[<Test>]
+let PositiveArgsPowersOfTwoTest () =
+    Assert.AreEqual(Some([32; 64; 128; 256]), powersOfTwoList 5 3)
+    Assert.AreEqual(Some([1; 2; 4; 8; 16; 32; 64; 128; 256; 512; 1024; 2048]), powersOfTwoList 0 11)
+    Assert.AreEqual(Some([8; 16; 32; 64]), powersOfTwoList 3 3)
--- Semester4/Homework1/1.4/1.4/PowersOfTwoList.fs
+++ Semester4/Homework1/1.4/1.4/PowersOfTwoList.fs
@@ -5,11 +5,11 @@ open System
 let powTwo n =
     let rec pow acc i =
         if n < 0 then
-            None
+            -1
         else if n = 0 then
-            Some(1)
+            1
         else if n = i then
-            Some(acc * 2)
+            acc * 2
         else
             pow (acc * 2) (i + 1)
 
@@ -19,11 +19,13 @@ let powersOfTwoList n m =
     let rec powersOfTwo acc i =
         if n < 0 || m < 0 then
             None
-        else if n = 0 && m = 0 then
+        else if n =0 && m = 0 then
             Some([1])
+        else if i = m + n then
+            powersOfTwo ((powTwo i) :: acc) (i - 1)
         else if i = n then
-            Some(acc)
+            Some((powTwo i) :: acc)
         else
             powersOfTwo ((List.head acc / 2) :: acc) (i - 1)
 
-    powersOfTwo ([]) (m)
+    powersOfTwo ([]) (m + n)
