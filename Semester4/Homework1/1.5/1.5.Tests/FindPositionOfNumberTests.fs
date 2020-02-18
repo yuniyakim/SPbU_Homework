fd76a3c2f68aa0d76d51cddfb6929952c8162a04
Yuniya Kim <kim.yuniya@mail.ru>
2020-02-19 00:20:48 +0300

    Added some tests

--- Semester4/Homework1/1.5/1.5.Tests/FindPositionOfNumberTests.fs
+++ Semester4/Homework1/1.5/1.5.Tests/FindPositionOfNumberTests.fs
@@ -4,5 +4,16 @@ open NUnit.Framework
 open FindPositionOfNumber
 
 [<Test>]
-let Test1 () =
-    Assert.Pass()
+let EmptyListFindPositionOfNumberTest () =
+    Assert.AreEqual(None, findPositionOfNumber [] 9)
+
+[<Test>]
+let NoNumberFindPositionOfNumberTest () =
+    Assert.AreEqual(None, findPositionOfNumber [24; -9; 0] 1624)
+    Assert.AreEqual(None, findPositionOfNumber [0; 0; 0; 0; 0; 0] 1)
+
+[<Test>]
+let FindPositionOfNumberTest () =
+    Assert.AreEqual(Some(1), findPositionOfNumber [-144; 18; -144; 0; -2; 34] -144)
+    Assert.AreEqual(None, findPositionOfNumber [18; -84; 0; 199; 10; 10; 93; 0] 5)
+    Assert.AreEqual(None, findPositionOfNumber [15; 0; 15; 0; 15; 0; 15; 15; 98] 9)
