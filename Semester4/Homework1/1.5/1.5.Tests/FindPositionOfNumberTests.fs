089b9c66e3df96751be35eedbb659e8d8398928d
Yuniya Kim <kim.yuniya@mail.ru>
2020-02-18 12:03:30 +0300

    Created 1.5

---
+++ Semester4/Homework1/1.5/1.5.Tests/FindPositionOfNumberTests.fs
@@ -0,0 +1,8 @@
+module FindPositionOfNumberTests
+
+open NUnit.Framework
+open FindPositionOfNumber
+
+[<Test>]
+let Test1 () =
+    Assert.Pass()
---
+++ Semester4/Homework1/1.5/1.5/FindPositionOfNumber.fs
@@ -0,0 +1,7 @@
+﻿module FindPositionOfNumber
+
+open System
+
+let findPositionOfNumber argv =
+    printfn "Hello World from F#!"
+    0 // return an integer exit code
