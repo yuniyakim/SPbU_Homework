26da2177c06376fb9c8b5044a0414e1e6f4ca828
Yuniya Kim <kim.yuniya@mail.ru>
2020-02-23 17:04:37 +0300

    Added comments and some testCases

--- Semester4/Homework2/2.1/2.1/AmountOfEvenNumbersFilter.fs
+++ Semester4/Homework2/2.1/2.1/AmountOfEvenNumbersFilter.fs
@@ -1,4 +1,5 @@
 ﻿module AmountOfEvenNumbersFilter
 
+/// Defines an amount of even numbers in list using filter
 let amountOfEvenNumbersFilter ls =
     ls |> List.filter (fun x -> x % 2 = 0) |> List.length 
