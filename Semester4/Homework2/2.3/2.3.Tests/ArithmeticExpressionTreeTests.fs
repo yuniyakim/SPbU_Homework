b45bd25a3d37863206332f292bb031bf27517aaa
Yuniya Kim <kim.yuniya@mail.ru>
2020-02-24 20:42:34 +0300

    Created 2.3, 2.3.Tests and updated appveyor and travis

---
+++ Semester4/Homework2/2.3/2.3.Tests/ArithmeticExpressionTreeTests.fs
@@ -0,0 +1,13 @@
+module ArithmeticExpressionTreeTests
+
+open NUnit.Framework
+open FsUnit
+open ArithmeticExpressionTree
+
+[<SetUp>]
+let Setup () =
+    ()
+
+[<Test>]
+let Test1 () =
+    Assert.Pass()
