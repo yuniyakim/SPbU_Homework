ba1741956ad8ffe8c211764d5b8accda841993a9
Yuniya Kim <kim.yuniya@mail.ru>
2020-02-24 14:55:21 +0300

    CopyPasted some code

---
+++ Semester4/Homework2/2.2/2.2/ApplyFunctionTree.fs
@@ -0,0 +1,30 @@
+﻿module ApplyFunctionTree
+
+type Tree<'a> =
+    | Node of 'a * Tree<'a> * Tree<'a>
+    | Empty
+
+type ContinuationStep<'a> =
+    | Finished
+    | Step of 'a * (unit -> ContinuationStep<'a>)
+
+let rec linearize binTree cont =
+    match binTree with
+    | Empty -> cont()
+    | Node(x, l, r) ->
+        Step(x, (fun () -> linearize l (fun () -> linearize r cont)))
+
+let iter f binTree =
+    let steps = linearize binTree (fun () -> Finished)
+
+    let rec processSteps step =
+        match step with
+        | Finished -> ()
+        | Step(x, getNext) ->
+            f x
+            processSteps (getNext())
+
+    processSteps steps
+
+let applyFunctionTree func tree = 
+    0
