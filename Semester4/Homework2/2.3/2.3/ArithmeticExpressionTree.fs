5664a1ffb6e7778aa7ae5fb247b0821ddbb0b110
Yuniya Kim <kim.yuniya@mail.ru>
2020-02-24 20:43:54 +0300

    Added AritmeticExpressionTree and calculate

--- Semester4/Homework2/2.3/2.3/ArithmeticExpressionTree.fs
+++ Semester4/Homework2/2.3/2.3/ArithmeticExpressionTree.fs
@@ -1,1 +1,20 @@
 ﻿module ArithmeticExpressionTree
+
+/// Arithmetic expression tree structure
+type AritmeticExpressionTree =
+    | Number of int
+    | Addition of AritmeticExpressionTree * AritmeticExpressionTree
+    | Subtraction of AritmeticExpressionTree * AritmeticExpressionTree
+    | Multiplication of AritmeticExpressionTree * AritmeticExpressionTree
+    | Division of AritmeticExpressionTree * AritmeticExpressionTree
+
+/// Calculates the result of arithmetic expression tree
+let rec calculate tree =
+    match tree with
+    | Number number -> number
+    | Addition(left, right) -> (calculate left) + (calculate right)
+    | Subtraction(left, right) -> (calculate left) - (calculate right)
+    | Multiplication(left, right) -> (calculate left) * (calculate right)
+    | Division(left, right) -> let rightNumber = calculate right
+                               if rightNumber = 0 then raise (System.DivideByZeroException())
+                               else (calculate left) / rightNumber
