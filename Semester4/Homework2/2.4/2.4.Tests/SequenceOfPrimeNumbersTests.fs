c5433401138a5bc9cbd487980ba99e58d1d2c63a
Yuniya Kim <kim.yuniya@mail.ru>
2020-02-26 00:49:10 +0300

    Improved isPrime and added some tests

--- Semester4/Homework2/2.4/2.4.Tests/SequenceOfPrimeNumbersTests.fs
+++ Semester4/Homework2/2.4/2.4.Tests/SequenceOfPrimeNumbersTests.fs
@@ -4,10 +4,25 @@ open NUnit.Framework
 open FsUnit
 open SequenceOfPrimeNumbers
 
-[<SetUp>]
-let Setup () =
-    ()
+let testCasesIsPrime = 
+    [
+        -19383, false
+        -4, false
+        -2, false
+        -1, false
+        0, false
+        1, false
+        2, true
+        3, true
+        9, false
+        11, true
+        53, true
+        91, false
+        113, true
+        117, false
+    ] |> List.map (fun (n, res) -> TestCaseData(n, res))
 
 [<Test>]
-let Test1 () =
-    Assert.Pass()
+[<TestCaseSource("testCasesIsPrime")>]
+let IsPrimeTest n res =
+    isPrime(n) |> should equal res
--- Semester4/Homework2/2.4/2.4/SequenceOfPrimeNumbers.fs
+++ Semester4/Homework2/2.4/2.4/SequenceOfPrimeNumbers.fs
@@ -2,8 +2,10 @@
 
 /// Checks if a number is prime
 let isPrime n =
-    let sqrt = (double >> sqrt >> int) n
-    [2 .. sqrt] |> List.forall (fun x -> n % x <> 0)
+    if n < 2 then false
+    else
+        let sqrt = (double >> sqrt >> int) n
+        [2 .. sqrt] |> List.forall (fun x -> n % x <> 0)
 
 /// Generates an infinite sequence of prime numbers
 let sequenceOfPrimeNumbers =
