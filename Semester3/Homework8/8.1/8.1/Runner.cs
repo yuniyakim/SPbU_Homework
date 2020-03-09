735992a7a7ace81325c7dd67a2f029ce592ec157
Yuniya Kim <kim.yuniya@mail.ru>
2020-03-10 01:22:33 +0300

    Added Lists

--- Semester3/Homework8/8.1/8.1/Runner.cs
+++ Semester3/Homework8/8.1/8.1/Runner.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using Attributes;
 using System.Collections.Concurrent;
+using System.IO;
+using System.Linq;
+using System.Reflection;
 
 namespace _8._1
 {
@@ -11,5 +14,16 @@ namespace _8._1
     {
         private ConcurrentQueue<Info> queue = new ConcurrentQueue<Info>();
         private Object lockObject = new Object();
+
+        public void Run(string path)
+        {
+            var files = Directory.GetFiles(path, "*.dll", SearchOption.AllDirectories)
+                .Where(x => x.Substring(x.LastIndexOf('\\') + 1) != "Attributes.dll");
+            foreach(var file in files)
+            {
+                var types = Assembly.LoadFrom(file).GetTypes();
+
+            }
+        }
     }
 }
