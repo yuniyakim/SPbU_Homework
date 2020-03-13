4e8cc212411eaacfd5fbf42b32dabfe712379897
Yuniya Kim <kim.yuniya@mail.ru>
2020-03-13 16:50:32 +0300

    Added checkBrackets

---
+++ Semester4/Homework4/4.1/4.1/Brackets.fs
@@ -0,0 +1,26 @@
+﻿module Brackets
+
+open System
+
+let checkBrackets str =
+    let strList = str |> Seq.toList
+    let length = strList |> List.length
+    let rec check stack acc =
+        if acc < length then
+            let element = List.item acc strList
+            if stack = [] then false
+            elif element = '(' || element = '[' || element = '{' then check (element :: stack) (acc + 1)
+            elif (element = ')' && List.head stack = '(') || (element = ']' && List.head stack = '[') || 
+                (element = '}' && List.head stack = '{') then check (List.tail stack) (acc + 1)
+            else false
+        List.isEmpty stack
+    check [] 0
+
+            //elif match List.head ls with
+            //| '(' | '[' | '{' -> check (List.head ls :: stack) (acc + 1)
+            //| ')' -> 
+            //    if List.head stack = '(' then check (List.tail stack) (acc + 1)
+            //| ']' -> 
+            //    if List.head stack = '[' then check (List.tail stack) (acc + 1)
+            //| '}' -> 
+            //    if List.head stack = '{' then check (List.tail stack) (acc + 1)
