3567d14fabc1c55fa5686fbde20b606e5eca0534
Yuniya Kim <kim.yuniya@mail.ru>
2020-03-15 16:56:59 +0300

    Added SimpleSuccessfulTests

---
+++ Semester3/Homework8/8.1/SimpleTests/SimpleSuccessfulTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Attributes;
+
+namespace SimpleSuccessfulTests
+{
+    public class SimpleSuccessfulTests
+    {
+        [Test]
+        public void SumTest()
+        {
+            var sum = 10 + 8;
+        }
+
+        [Test]
+        public void SubTest()
+        {
+            var sub = 239 - 107;
+        }
+    }
+}
