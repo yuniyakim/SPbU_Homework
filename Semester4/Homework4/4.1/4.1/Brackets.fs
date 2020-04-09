4acbb532d2cc5b4e440bd0d02b3898a7151e6df7
Yuniya Kim <kim.yuniya@mail.ru>
2020-04-09 18:32:28 +0300

    Fixed a bug and deleted extra code

--- Semester4/Homework4/4.1/4.1/Brackets.fs
+++ Semester4/Homework4/4.1/4.1/Brackets.fs
@@ -2,9 +2,6 @@
 
 open System
 
-let isClosingBracket (bracket: char) =
-    bracket = ')' || bracket = ']' || bracket = '}'
-
 let checkBrackets (str: string) =
     let rec check (str: List<char>) (stack: List<char>) =
         if str = [] then
@@ -13,38 +10,8 @@ let checkBrackets (str: string) =
             | _ -> false
         else
             let strHead = List.head str
-            let stackHead = List.head stack
             if (strHead = '(' || strHead = '[' || strHead = '{') then check (List.tail str) (strHead :: stack)
-            elif ((strHead = ')' && stackHead = '(') || (strHead = ']' && stackHead = '[') || 
-                        (strHead = '}' && stackHead = '{')) then check (List.tail str) (List.tail stack)
+            elif (not (List.isEmpty stack) && ((strHead = ')' && List.head stack = '(') || (strHead = ']' && List.head stack = '[') || 
+                        (strHead = '}' && List.head stack = '{'))) then check (List.tail str) (List.tail stack)
             else false
-            //match strHead with
-            //| '(' | '[' | '{' -> check (List.tail str) (strHead :: stack)
-            //| ')' when stackHead = '(' -> check (List.tail str) (List.tail stack)
-            //| ']' when stackHead = '[' -> check (List.tail str) (List.tail stack)
-            //| '}' when stackHead = '{' -> check (List.tail str) (List.tail stack)
     check (Seq.toList str) []
-
-
-
-    //let strList = str |> Seq.toList
-    //let length = strList |> List.length
-    //let rec check stack acc =
-    //    if acc < length then
-    //        let element = List.item acc strList
-    //        if stack = [] then false
-    //        elif element = '(' || element = '[' || element = '{' then check (element :: stack) (acc + 1)
-    //        elif (element = ')' && List.head stack = '(') || (element = ']' && List.head stack = '[') || 
-    //            (element = '}' && List.head stack = '{') then check (List.tail stack) (acc + 1)
-    //        else false
-    //    List.isEmpty stack
-    //check [] 0
-
-            //elif match List.head ls with
-            //| '(' | '[' | '{' -> check (List.head ls :: stack) (acc + 1)
-            //| ')' -> 
-            //    if List.head stack = '(' then check (List.tail stack) (acc + 1)
-            //| ']' -> 
-            //    if List.head stack = '[' then check (List.tail stack) (acc + 1)
-            //| '}' -> 
-            //    if List.head stack = '{' then check (List.tail stack) (acc + 1)
