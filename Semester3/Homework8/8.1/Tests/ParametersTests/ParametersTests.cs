16543dd2d395212df5f462cc801775146095a417
Yuniya Kim <kim.yuniya@mail.ru>
2020-04-25 18:08:58 +0300

    Renamed folders

---
+++ Semester3/Homework8/8.1/Tests/ParametersTests/ParametersTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Attributes;
+
+
+namespace ParametersTests
+{
+    public class ParametersTests
+    {
+        [Test]
+        public void OneParameterTest(int number)
+        {
+            number += 12;
+        }
+
+        [Test]
+        public void TwoParametersTest(int number1, int number2)
+        {
+            number1 *= number2;
+        }
+    }
+}
---
+++ Semester3/Homework8/SimpleFailedTests/SimpleFailedTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Attributes;
+
+namespace SimpleFailedTests
+{
+    public class SimpleFailedTests
+    {
+        public int Number = 0;
+
+        [Before]
+        public void Before()
+        {
+            Number += 12;
+        }
+
+        [AfterClass]
+        public void AfterClass()
+        {
+            Number -= 0;
+        }
+
+        [Test]
+        public void FirstTest()
+        {
+            Number /= 2;
+            throw new Exception();
+        }
+
+        [Test]
+        public void SecondTest()
+        {
+            throw new Exception();
+        }
+    }
+}
