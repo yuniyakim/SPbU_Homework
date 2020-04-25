61106f7d567ad49ad15260add4dc6fa8369dea6e
Yuniya Kim <kim.yuniya@mail.ru>
2020-04-25 22:02:47 +0300

    Added CheckInfo

--- Semester3/Homework8/8.1/8.1.Tests/Tests.cs
+++ Semester3/Homework8/8.1/8.1.Tests/Tests.cs
@@ -1,4 +1,6 @@
 using NUnit.Framework;
+using System;
+using System.Collections;
 using System.IO;
 
 namespace _8._1.Tests
@@ -9,9 +11,16 @@ namespace _8._1.Tests
         private string path = new FileInfo(System.Reflection.Assembly.GetExecutingAssembly().Location).DirectoryName + "../../../../../Tests";
         private Runner runner;
 
-        private void CheckInfo()
+        private void CheckInfo(Info info, string name, string result, string ignoreReason)
         {
+            Assert.AreEqual(name, info.Name);
+            Assert.AreEqual(result, info.Result);
+            Assert.AreEqual(ignoreReason, info.IgnoreReason);
+        }
 
+        public static int Compare(Info info1, Info info2)
+        {
+            return new CaseInsensitiveComparer().Compare(info1.Name, info2.Name);
         }
 
         [SetUp]
@@ -25,6 +34,10 @@ namespace _8._1.Tests
         {
             var info = runner.Run(path + "/SimpleSucceededTests");
             Assert.AreEqual(3, info.Length);
+            Array.Sort(info, Compare);
+            CheckInfo(info[0], "FirstTest", "Passed", null);
+            CheckInfo(info[1], "SecondTest", "Passed", null);
+            CheckInfo(info[2], "ThirdTest", "Passed", null);
         }
 
         [Test]
@@ -32,6 +45,9 @@ namespace _8._1.Tests
         {
             var info = runner.Run(path + "/SimpleFailedTests");
             Assert.AreEqual(2, info.Length);
+            Array.Sort(info, Compare);
+            CheckInfo(info[0], "FirstTest", "Failed", "The test has thrown the System.Exception.");
+            CheckInfo(info[1], "SecondTest", "Failed", "The test has thrown the System.Exception.");
         }
 
         [Test]
@@ -39,6 +55,9 @@ namespace _8._1.Tests
         {
             var info = runner.Run(path + "/ExceptionTests");
             Assert.AreEqual(2, info.Length);
+            Array.Sort(info, Compare);
+            CheckInfo(info[0], "DivideByZeroExceptionTest", "Passed", null);
+            CheckInfo(info[1], "IndexOutOfRangeExceptionTest", "Passed", null);
         }
 
         [Test]
@@ -46,6 +65,12 @@ namespace _8._1.Tests
         {
             var info = runner.Run(path + "/ParametersTests");
             Assert.AreEqual(5, info.Length);
+            Array.Sort(info, Compare);
+            CheckInfo(info[0], "OneIntParameterTest", "Passed", null);
+            CheckInfo(info[1], "OneStringParameterTest", "Passed", null);
+            CheckInfo(info[2], "TwoDifferentParametersTest", "Passed", null);
+            CheckInfo(info[3], "TwoIntParametersTest", "Passed", null);
+            CheckInfo(info[4], "TwoStringParametersTest", "Passed", null);
         }
 
         [Test]
@@ -53,6 +78,9 @@ namespace _8._1.Tests
         {
             var info = runner.Run(path + "/IgnoreTests");
             Assert.AreEqual(2, info.Length);
+            Array.Sort(info, Compare);
+            CheckInfo(info[0], "FirstIgnoreTest", "Ignored", "First ignore test");
+            CheckInfo(info[0], "SecondIgnoreTest", "Ignored", "Second ignore test");
         }
     }
 }
---
+++ Semester3/Homework8/8.1/8.1/Printer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _8._1
+{
+    public class Printer
+    {
+        /// <summary>
+        /// Print the results of tests
+        /// <param name="info"/>Info about tests</param>
+        /// </summary>
+        public static void PrintResults(Info[] info)
+        {
+            foreach (var testInfo in info)
+            {
+                Console.Write("Name: " + testInfo.Name);
+                Console.Write(" | Result: " + testInfo.Result);
+
+                if (testInfo.Result != "Passed")
+                {
+                    Console.Write($" | Ignore reason: {testInfo.IgnoreReason}");
+                }
+
+                Console.WriteLine($" | time: {testInfo.Time}");
+            }
+        }
+    }
+}
