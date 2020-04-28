a5b273f52cf712274eaaed8e9f57bc993207ad14
Yuniya Kim <kim.yuniya@mail.ru>
2020-04-28 14:56:36 +0300

    Added UploadFile and upload form

--- Semester3/Homework12/12.1/12.1/Controllers/HomeController.cs
+++ Semester3/Homework12/12.1/12.1/Controllers/HomeController.cs
@@ -1,16 +1,57 @@
 ﻿using _12._1.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
+using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace _12._1.Controllers
 {
+    /// <summary>
+    /// Web-app controller
+    /// </summary>
     public class HomeController : Controller
     {
+        private History history;
+        private IWebHostEnvironment environment;
+
+        /// <summary>
+        /// Controller's constructor
+        /// </summary>
+        /// <param name="history">History of assemblies and tests</param>
+        /// <param name="environment">Web environment</param>
+        public HomeController(History history, IWebHostEnvironment environment)
+        {
+            this.history = history;
+            this.environment = environment;
+        }
+
+        /// <summary>
+        /// Loads start page
+        /// </summary>
         public IActionResult Index()
         {
-            ViewBag.Time = DateTime.Now;
-            return View("MyView");
+            return View("TestRunner");
+        }
+
+        /// <summary>
+        /// Uploads file to Files folder
+        /// </summary>
+        /// <param name="file">Added file</param>
+        [HttpPost]
+        public IActionResult UploadFIle(IFormFile file)
+        {
+            if (file != null)
+            {
+                using (var fileStream = new FileStream(environment.WebRootPath + "/Files/" + file.FileName, FileMode.Create))
+                {
+                    file.CopyTo(fileStream);
+                }
+            }
+
+            return RedirectToAction("Index");
         }
 
         [HttpGet]
--- Semester3/Homework12/12.1/12.1/Models/History.cs
+++ Semester3/Homework12/12.1/12.1/Models/History.cs
@@ -1,9 +1,4 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System.Data.Entity;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace _12._1.Models
 {
