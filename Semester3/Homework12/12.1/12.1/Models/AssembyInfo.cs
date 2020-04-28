a700f4316aee7ac9158e03213858cf9e16019cf9
Yuniya Kim <kim.yuniya@mail.ru>
2020-04-28 13:55:16 +0300

    Added AssemblyInfo

---
+++ Semester3/Homework12/12.1/12.1/Models/AssembyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace _12._1.Models
+{
+    /// <summary>
+    /// Assembly's info
+    /// </summary>
+    public class AssembyInfo
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<TestInfo> Tests { get; set; }
+    }
+}
