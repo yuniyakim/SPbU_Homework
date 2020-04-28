942035d20cd6e8c101773b4a8ff25dd0d82bf6af
Yuniya Kim <kim.yuniya@mail.ru>
2020-04-28 12:37:41 +0300

    Added 8.1

---
+++ Semester3/Homework12/8.1/Attributes/Before.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Attributes
+{
+    /// <summary>
+    /// Attribute for methods which must be executed before every test in a class
+    /// </summary>
+    public class Before : Attribute
+    {
+    }
+}
