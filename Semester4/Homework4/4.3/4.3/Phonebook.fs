8f00f4ea8975e8dfaa987ec85925b43d3d495588
Yuniya Kim <kim.yuniya@mail.ru>
2020-05-04 01:07:05 +0300

    Added printAllContacts and addContact

--- Semester4/Homework4/4.3/4.3/Phonebook.fs
+++ Semester4/Homework4/4.3/4.3/Phonebook.fs
@@ -1,15 +1,29 @@
 ﻿module Phonebook
 
+open System
+
+/// Prints menu
 let printMenu () =
     printf "Menu:"
     printf "1 – exit"
     printf "2 – add contact"
     printf "3 – find number by name"
     printf "4 – find name by number"
-    printf "5 – show all contacts"
+    printf "5 – print all contacts"
     printf "6 – save data to file"
     printf "7 – read data from file"
 
+/// Prints all the contacts
+let printAllContacts (list: List<string * string>) =
+    List.map (fun (name, number) -> printf "Name: %s, number: %s" name number) list
+
+/// Adds a new contact with entered name and number
+let addContact (list: List<string * string>) =
+    printf "Enter name"
+    let name = Console.ReadLine()
+    printf "Enter number"
+    let number = Console.ReadLine()
+    list @ [(name, number)]
 
 //Написать программу - телефонный справочник. Она должна уметь хранить имена и номера телефонов, в интерактивном режиме осуществлять следующие операции:
 //1. выйти
