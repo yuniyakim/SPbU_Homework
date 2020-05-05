dd84420faae112d8a0a611750fca4f9bc04c57e1
Yuniya Kim <kim.yuniya@mail.ru>
2020-05-05 16:55:11 +0300

    Added 6.1 and Rounder

---
+++ Semester4/Homework6/6.1/6.1/Rounder.fs
@@ -0,0 +1,9 @@
+﻿module Rounder
+
+open System
+
+type Rounder(accuracy: int) =
+    member this.Bind(x: float, f) =
+        f <| Math.Round(x, accuracy)
+    member this.Return(x: float) =
+        Math.Round(x, accuracy)
