16cf133cbe426469e37fc3ba84da000bd057dd4b
Yuniya Kim <kim.yuniya@mail.ru>
2020-06-06 22:43:21 +0300

    Added Computer

--- Semester4/Homework5/5.1/5.1/Computer.fs
+++ Semester4/Homework5/5.1/5.1/Computer.fs
@@ -1,2 +1,7 @@
 ﻿module Computer
 
+/// Computer's type
+type Computer(name: string, os: string, infected: bool) =
+    member this.Name = name
+    member this.OS = os
+    member val Infected = infected with get, set
